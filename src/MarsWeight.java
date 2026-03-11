a7d9b4a1d7cbe4de719d6ef863fb073a983670ba
abdal <abdallahm3400@gmail.com>
2026-03-11 14:28:26 +0100

    Initial Commit

---
+++ src/MarsWeight.java
@@ -0,0 +1,9 @@
+public class MarsWeight {
+    // --- Variable declarations ---
+    static float earthWeightAbdalla;
+    static float marsWeightAbdalla;  // calculated weight on Mars (in kg)
+    static double marsWeightDoubleAbdalla; // as double
+    static int marsWeightIntAbdalla; // Mars weight after casting to int
+    static char marsWeightCharAbdalla; //as a char
+    static int finalValueAbdalla;  // result after doing math on char
+}
